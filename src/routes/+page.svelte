59ed90b41b3a8cf9a0527916c2ac147924ede933
Rx-78 <149854255+rx-72@users.noreply.github.com>
2024-02-28 13:56:07 -0800

    Add files via upload

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,9 @@
 <script>
-    import App from '../components/App.svelte';
+    import ScrollyTeller from '../components/ScrollyTeller.svelte'
   </script>
   
-  <App />
+  <main>
+    <ScrollyTeller />
+</main>
+
   
